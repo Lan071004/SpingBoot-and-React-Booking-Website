719cbbcaa500efa21fd8e42a29fefc0a6f7f3ac2
Lan071004 <lanie.071004@gmail.com>
2025-04-08 00:43:56 +0700

    Initial IWS Final Project with setting up Springboot and React

---
+++ booking-api/src/test/java/project/BookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package iws.bookingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
